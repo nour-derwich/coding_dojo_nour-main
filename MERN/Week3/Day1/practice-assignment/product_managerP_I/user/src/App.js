d3d2a86d887f3b9ac8fad5d98ca204fa04d73a5e
nour-logstic <88410368+nour-derwich@users.noreply.github.com>
2022-12-07 16:36:13 +0100

    Product Manager

--- MERN/Week3/Day1/practice-assignment/product_managerP_I/user/src/App.js
+++ MERN/Week3/Day1/practice-assignment/product_managerP_I/user/src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
 import Main from './components/Main';
+import Display from './components/Display';
+import {Route ,Routes} from 'react-router-dom'
+import OneProd from './components/OneProd';
 function App() {
   return (
     <div className="App">
         <h1>Product Manager</h1>
-      <Main/>
+      
+      <hr/>
+      <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path="/prodect/:id"element={<OneProd/>}/>
+      <Route path="/display" element={<Display/>} />
+   
+     </Routes>
     </div>
   );
 }
