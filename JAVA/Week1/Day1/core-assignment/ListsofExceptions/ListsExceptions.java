b3017bf755f7e3f13277ed1ac1ae4261748bc20c
nour-logstic <88410368+nour-derwich@users.noreply.github.com>
2022-12-21 19:14:44 +0100

    ListsExceptions

---
+++ JAVA/Week1/Day1/core-assignment/ListsofExceptions/ListsExceptions.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class ListsExceptions {
+  public static void main(String[] args) {
+    ArrayList<Object> list = new ArrayList<>();
+    list.add("13");
+    list.add("hello world");
+    list.add(25);
+    list.add("Goodbye World");
+
+    
+
+    // Try to cast each element to an Integer
+    for (Object element : list) {
+      try {
+        
+        Integer num = (Integer) element;
+        System.out.println(num);
+      } catch (ClassCastException e) {
+        System.out.println("" + element + "");
+      }
+    }
+  }
+}
--- JAVA/Week1/Day1/optional-assignment/MapoftheHashmatique/TestHanshMap.java
+++ JAVA/Week1/Day1/optional-assignment/MapoftheHashmatique/TestHanshMap.java
@@ -1,8 +1,13 @@
-public class TestHanshMap {
-    public static void main(String[]args)
-    {
-       System.out.println(userMap());
-
-
+import java.util.Random;
+class DeliverMessage{
+    public static void main(String[] args){
+        UnreliableFriend friend = new UnreliableFriend();
+        try{
+            friend.deliverMessage();
+            System.out.println("The message was delivered!");
+        } catch (OutOfGasException e){
+            System.out.println("Hey, uh, so, I ran out of gas..");
+            // back-up plan here.
+        }
     }
 }
