fa02896ab05417016f46f34cc1111d613653196c
nour-logstic <88410368+nour-derwich@users.noreply.github.com>
2022-12-26 23:12:31 +0100

    hopper_receipt

---
+++ JAVA/Hopper_Receipt/src/test/java/com/nour/Hopper_Receipt/HopperReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nour.Hopper_Receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopperReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
